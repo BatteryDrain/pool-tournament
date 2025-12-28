cb927ce816ad0b5432cc188aca930d1403778e44
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-12-28 13:16:40 -0700

    fixed errors

--- fetch.js
+++ fetch.js
@@ -29,17 +29,6 @@ function csvToBIGARRAY(csvString) {
         DATASORTED.push(DATA[d]);
     }
 
-    for(i=1; i<DATASORTED.length; i++){
-        temp = [parseInt(DATASORTED[i][0])];
-        for(j=0; j<5; j++){
-            if(DATASORTED[i][j + 10] != ''){
-                temp.push(parseInt(DATASORTED[i][j + 10]));
-            }
-        }
-        MOVIETAGS.push(temp);
-    }
-    }
-
     loadScript("DATA2.js", () => {
         loadScript("script.js");
       });
