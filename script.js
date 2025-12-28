a961e9347c79803df34ae52eb5c152bcc32fb4ec
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-12-28 14:25:03 -0700

    made table

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-console.log("❌✅");
+console.log("in script.js");
 build();
 function build(){
-
+    for(j=0; j<DATASORTED.length; j++){
+        row = document.createElement("li");
+        for(i=0; i<5; i++){
+            th = document.createElement("th");
+            text = DATASORTED[j][i];
+            if(text == "W"){
+                th.innerHTML = "✅";
+            } 
+            else{
+                th.innerHTML = text;
+            }
+            if(text == "L"){
+                th.innerHTML = "❌";
+            }
+            row.appendchild(th);
+        }
+        out.appendchild(row);
+    }
 }
